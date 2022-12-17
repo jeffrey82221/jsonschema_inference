59bbe1c558b3d71262c927d924d15ea0a7d47da2
jeffrey82221 <jeffrey82221@gmail.com>
2022-12-17 20:03:51 +0800

    fix: flake8

--- examples/new_jsonl.py
+++ examples/new_jsonl.py
@@ -9,6 +9,7 @@ from jsonschema_inference.schema.objs import Union
 
 __all__ = ['JsonlInferenceEngine']
 
+
 def get_schema_remotely(jsonl_path, verbose=True, position=0):
     import json
     import tqdm
@@ -22,22 +23,24 @@ def get_schema_remotely(jsonl_path, verbose=True, position=0):
         schema = InferenceEngine.get_schema(json_pipe)
     return schema
 
+
 class JsonlInferenceEngine:
     """
     Args:
         - inference_worker_cnt: number of processes inferencing the json schema
-        - tmp_dir: the path to store the splitted jsonl files. 
+        - tmp_dir: the path to store the splitted jsonl files.
     Methods to be overide:
         - jsonl_path: path to the jsonl file.
     """
+
     def __init__(self, inference_worker_cnt=8, tmp_dir='/tmp'):
         self._inference_worker_cnt = inference_worker_cnt
         self._tmp_dir = tmp_dir
-        # NOTE: Whether the engine is pypy or python depends on how the code is executed 
-        self._engine = 'pypy' # or python
+        # NOTE: Whether the engine is pypy or python depends on how the code is
+        # executed
+        self._engine = 'pypy'  # or python
         signal.signal(signal.SIGTERM, self._remove_split_files)
         signal.signal(signal.SIGINT, self._remove_split_files)
-
 
     @abc.abstractproperty
     def jsonl_path(self):
@@ -52,22 +55,28 @@ class JsonlInferenceEngine:
     def get_schema_parallel(self, verbose=True):
         self._split_jsonl(self._inference_worker_cnt)
         schemas = []
+
         def layered_get_schema(i, filename):
             if self._engine == 'pypy':
                 decorated_get_schema = remote.pypy(get_schema_remotely)
             else:
                 decorated_get_schema = remote.python(get_schema_remotely)
-            schema = decorated_get_schema(filename, verbose=verbose, position=i)
+            schema = decorated_get_schema(
+                filename, verbose=verbose, position=i)
             schemas.append(schema)
         try:
             split_file_paths = os.listdir(self._split_path)
-            split_file_pipe = map(lambda file: os.path.join(self._split_path, file), split_file_paths)
+            split_file_pipe = map(
+                lambda file: os.path.join(
+                    self._split_path,
+                    file),
+                split_file_paths)
             # construct the threads
             threads = [Thread(target=layered_get_schema, args=(i, filename))
-                    for i, filename in enumerate(split_file_pipe)]
+                       for i, filename in enumerate(split_file_pipe)]
             # start the threads
             for thread in threads:
-                thread.daemon=False
+                thread.daemon = False
                 thread.start()
             # wait for the threads to complete
             for i, thread in enumerate(threads):
@@ -89,8 +98,9 @@ class JsonlInferenceEngine:
         total = int(out.decode("utf-8").split(self.jsonl_path)[0])
         if not os.path.exists(self._split_path):
             os.mkdir(self._split_path)
-        json_cnt_per_file = int(total/split_count)
-        subprocess.run(['split', '-l', str(json_cnt_per_file), self.jsonl_path, self._split_path + '/'])
+        json_cnt_per_file = int(total / split_count)
+        subprocess.run(['split', '-l', str(json_cnt_per_file),
+                       self.jsonl_path, self._split_path + '/'])
         return json_cnt_per_file
 
     def _remove_split_files(self, signal=None, frame=None):
@@ -98,7 +108,7 @@ class JsonlInferenceEngine:
             subprocess.run(['rm', '-r', self._split_path])
             print(self._split_path, 'removed')
         os._exit(1)
-        
+
     @property
     def _split_path(self):
         return os.path.join(self._tmp_dir, 'split')
@@ -108,6 +118,7 @@ class Engine(JsonlInferenceEngine):
     @property
     def jsonl_path(self):
         return 'data/kaggle_data/test.jsonl'
+
 
 if __name__ == '__main__':
     engine = Engine(inference_worker_cnt=1)
--- examples/remote.py
+++ examples/remote.py
@@ -3,6 +3,7 @@ import inspect
 from functools import wraps
 from jsonschema_inference.schema.objs import *
 
+
 def build_remote(gw, function, engine='pypy'):
     func_name = function.__name__
     consumer_str = f"""
@@ -14,17 +15,21 @@ channel.send(repr({func_name}(*args, **kwargs)))
     channel = gw.remote_exec(consumer_str)
     return channel
 
+
 def wrap(func, channel):
     @wraps(func)
     def wrapped_func(*args, **kwargs):
         channel.send((args, kwargs))
         return eval(channel.receive())
     return wrapped_func
+
+
 def pypy(func):
     gw = execnet.makegateway("popen// pypy3")
     channel = build_remote(gw, func, engine='pypy')
     wrapped_func = wrap(func, channel)
     return wrapped_func
+
 
 def python(func):
     gw = execnet.makegateway("popen// python")
