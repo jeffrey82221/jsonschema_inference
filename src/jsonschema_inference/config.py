551dc80aa6417ff0c53673efef2a059da07df891
jeffrey82221 <jeffrey82221@gmail.com>
2023-01-15 11:00:57 +0800

    refactor: doc

--- src/jsonschema_inference/config.py
+++ src/jsonschema_inference/config.py
@@ -1,11 +1,13 @@
 """
 Config the Json Schema Inference Model
-- 1. equivalence model: kind | label 
-    - kind: Turn on DynamicRecord for unifing Records with different fields
-    - label: Turn off DynamicRecord and construct Union of Records with inconsistent fields
-- 2. enable_uniform_record: True | False (whether try to unify Record values for Record with values of same kind)
 """
 class Config:
+    """
+    - 1. equivalence model: kind | label 
+        - kind: Turn on DynamicRecord for unifing Records with different fields
+        - label: Turn off DynamicRecord and construct Union of Records with inconsistent fields
+    - 2. enable_uniform_record: True | False (whether try to unify Record values for Record with values of same kind)
+    """
     def __init__(self, unify_records = True, equivalence_mode='kind'):
         self.init(unify_records=unify_records, equivalence_mode=equivalence_mode)
 
