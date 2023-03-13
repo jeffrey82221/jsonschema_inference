9d38755d6fbe39e7c5227714c8c4957773ab6be6
jeffrey82221 <jeffrey82221@gmail.com>
2023-03-13 22:53:53 +0800

    fix: mypy

--- jsonschema_inference/schema/inference/base.py
+++ jsonschema_inference/schema/inference/base.py
@@ -1,7 +1,7 @@
 """
 A basic json schema inference engine
 """
-from typing import List, Iterable
+import typing
 from ..fitter import fit
 from ..objs import JsonSchema
 from .reduce import reduce_schema
@@ -14,13 +14,13 @@ class InferenceEngine:
     def __init__(self, batch_size=100):
         self._batch_size = batch_size
 
-    def get_schema_iteratively(self, json_pipe: Iterable):
+    def get_schema_iteratively(self, json_pipe: typing.Iterable[typing.Any]):
         batch_pipe = InferenceEngine._batchwise_generator(json_pipe, batch_size=self._batch_size)
         schema_pipe = map(lambda batch: InferenceEngine.get_schema(batch), batch_pipe)
         return reduce_schema(schema_pipe)
     
     @staticmethod
-    def get_schema(json_batch: List) -> JsonSchema:
+    def get_schema(json_batch: typing.List[typing.Any]) -> JsonSchema:
         return reduce_schema(
             map(fit, json_batch))
     
